38abb8738a60ac1586a708dca7dcfc088adc5f12
YatagacLapotk <you@example.com>
2026-01-25 23:44:22 +0300

    .

---
+++ CPU/TEST_BENCH/REG_FILE_TB.v
@@ -0,0 +1,13 @@
+module REG_FILE_TB ();
+
+wire          clk;
+wire          res;
+wire [4:0]    A1;
+wire [4:0]    A2;
+wire [4:0]    A3;
+wire [31:0]   WD;
+reg  [31:0]   RD1;
+reg  [31:0]   RD2;
+
+    
+endmodule
