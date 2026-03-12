e278857968d2ba507db59550f1bb15de75a121f1
YatagacLapotk <you@example.com>
2026-03-12 03:32:08 +0300

    EXECUTE biraz daha ilerletildi.

--- CPU/CORE/EXECUTE.v
+++ CPU/CORE/EXECUTE.v
@@ -28,9 +28,37 @@ module EXECUTE (
     output [`ADDRESS_WIDTH-1:0] rdM,
     output [`DATA_WIDTH-1:0] pc_target_out,
     output pc_src,
-    output [`DATA_WIDTH-1:0]alu_result_out
+    output [`DATA_WIDTH-1:0]result_out
 );
 
-//Giriş çıkışı tanımlayayım dedim, başlangıcı atalım diye. DECODE'a da bir kaç ekleme yaptım. 
+//Biraz daha devam ettirdim. küçük küçk devam ettiriyorum.
+
+wire [`DATA_WIDTH-1:0] alu_src_A;
+wire [`DATA_WIDTH-1:0] alu_src_B;
+wire [`DATA_WIDTH-1:0] mdu_src_A;
+wire [`DATA_WIDTH-1:0] mdu_src_B;
+wire [`DATA_WIDTH-1:0] alu_result_out;
+wire [`DATA_WIDTH-1:0] mdu_result_out;
+
+
+ALU alu(
+    .s1(alu_src_A),
+    .s2(alu_src_B),
+    .ALU_CNTR(alu_control),
+    .ALU_OUT(alu_result_out)
+);
+
+MDU mdu(
+    .s1(mdu_src_A),
+    .s2(mdu_src_B),
+    .funct3(mdu_control),
+    .d3(mdu_result_out)
+);
+
+always @(*) begin
+    
+end
+
+
     
 endmodule
