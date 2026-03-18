8aadf98b1a62d01201788bb8eaba5350c3ede7ba
Frksy <faruk_fr@outlook.com>
2026-03-18 16:49:58 +0300

    EXECUTE eklemeler yapıldı, imm ataması ve isa select eklendi.

--- CPU/CORE/EXECUTE.v
+++ CPU/CORE/EXECUTE.v
@@ -5,7 +5,7 @@ module EXECUTE (
     input [`DATA_WIDTH-1:0] rd1,
     input [`DATA_WIDTH-1:0] rd2,
     input [`DATA_WIDTH-1:0] pc,
-    input [`DATA_WIDTH-1:0] imm_ext,
+    input [`DATA_WIDTH-1:0] imm,
     input [`DATA_WIDTH-1:0] alu_result_in,
     input [`DATA_WIDTH-1:0] wb_in,
     input [`ADDRESS_WIDTH-1:0] rs1_addr_in,
@@ -28,7 +28,7 @@ module EXECUTE (
     output [`ADDRESS_WIDTH-1:0] rdM,
     output [`DATA_WIDTH-1:0] pc_target_out,
     output pc_src,
-    output [`DATA_WIDTH-1:0]result_out
+    output reg [`DATA_WIDTH-1:0]result_out
 );
 
 //Biraz daha devam ettirdim. küçük küçk devam ettiriyorum.
@@ -55,10 +55,27 @@ MDU mdu(
     .d3(mdu_result_out)
 );
 
+assign pc_target_out = pc + imm; //Branch ve jump işlemleri için hedef adres ataması.
+assign mdu_src_A = rd1;
+assign mdu_src_B = rd2;
+
+//Immidiate işlemleri için kaynak atama.
 always @(*) begin
-    
+    if (alu_imm_en) begin
+        alu_src_A = rd1;
+        alu_src_B = imm;
+    end else begin
+        alu_src_A = rd1;
+        alu_src_B = rd2;
+    end
 end
-
-
-    
+always @ (*) begin
+    if (isa_slct == 2'b00) 
+        result_out = alu_result_out;
+    else if (isa_slct == 2'b01) 
+        result_out = mdu_result_out;
+    else if (isa_slct == 2'b10) // jal ve jalr için ama jalr için pc'nin rs1 + imm olması gerekiyor sanırım emin değilim. 
+        result_out = pc + 4; 
+    else result_out = 32'b0;
+end
 endmodule
