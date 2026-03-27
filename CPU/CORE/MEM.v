30899ea510daab34043be3ccd957d48568bf98f4
YatagacLapotk <you@example.com>
2026-03-28 00:01:01 +0300

    MEM başlandı.

---
+++ CPU/CORE/MEM.v
@@ -0,0 +1,10 @@
+`include "/Users/yatagaclapotk/Desktop/Genel_Calismalar/Mezuniyet/Mezuniyet_Projesi/CPU/SABIT_VERILER/sabit_veriler.vh"
+
+module MEM (
+    input clk,
+    input reset,
+    input [`DATA_WIDTH-1:0] execute_result_in,
+    output [`DATA_WIDTH-1:0] execute_result_out
+);
+    
+endmodule
