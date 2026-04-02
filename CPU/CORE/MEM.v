ca0f44c30c7907f20dd2b057211810ac4e0b57c5
YatagacLapotk <you@example.com>
2026-04-02 13:19:25 +0300

    MEM başlandı.

--- CPU/CORE/MEM.v
+++ CPU/CORE/MEM.v
@@ -4,6 +4,11 @@ module MEM (
     input clk,
     input reset,
     input [`DATA_WIDTH-1:0] execute_result_in,
+    input [`DATA_WIDTH-1:0] mem_write_data,
+    input reg_write,
+    input mem_write,
+    input [`ADDRESS_WIDTH-1:0] rdM,
+    output [`ADDRESS_WIDTH-1:0] rdW,
     output [`DATA_WIDTH-1:0] execute_result_out
 );
     
