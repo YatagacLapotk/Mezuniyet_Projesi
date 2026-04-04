3ad7f1bfafed7fd09f41cd4c83d86e384c9421e8
YatagacLapotk <you@example.com>
2026-04-04 22:51:55 +0300

    I_cache bitirildi. Testleri yapılacak.

--- CPU/CACHE/I_CACHE.v
+++ CPU/CACHE/I_CACHE.v
@@ -8,37 +8,25 @@ input we,
 input [`INSTRUCTION_WIDTH-1:0] inst_in,
 input [`CACHE_ADDRESS-1:0] w_addr,
 
-//FETCH inst request
-input instruction_req,
+//FETCH inst
 input [`CACHE_ADDRESS-1:0] r_addr,
-output reg [`INSTRUCTION_WIDTH-1:0] inst_out,
-output hit,
-
-// Memory miss request 
-output mem_req,
-output reg [`INSTRUCTION_WIDTH-1:0] mem_address,         
-input wire [`INSTRUCTION_WIDTH-1:0] mem_data_in,         
-input wire mem_ready 
+output [`INSTRUCTION_WIDTH-1:0] inst_out
 );
 
 reg [`INSTRUCTION_WIDTH-1:0] i_cache [0:`CACHE_SIZE]; // cache boyutunu 8kb olara ayarladım sonrasında konuşur değişiriz.
 
 always @(posedge clk) begin
     if (reset)begin
-        for(integer i = 0; i<`INSTRUCTION_WIDTH-1;i+=1)begin
-        
+        for(integer i = 0; i<`CACHE_SIZE;i+=1)begin
+            i_cache[i]<= 0;
         end
     end 
-    if (we)begin
+    else if (we)begin
         i_cache[w_addr] = inst_in;
     end
-    else if (instruction_req)begin
-        inst_out <= i_cache[r_addr];
-    end
-
-
 end
 
+assign inst_out = i_cache[r_addr];
 
 
     
