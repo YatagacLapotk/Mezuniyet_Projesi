f75e952c5aba2a2f7522102a037629fdd5d0af91
YatagacLapotk <you@example.com>
2026-04-05 16:18:20 +0300

    Hazard unit eklendi ve başlandı.

---
+++ CPU/CORE/HAZARD_UNIT.v
@@ -0,0 +1,11 @@
+`include "/Users/yatagaclapotk/Desktop/Genel_Calismalar/Mezuniyet/Mezuniyet_Projesi/CPU/SABIT_VERILER/sabit_veriler.vh"
+
+
+module HU (  
+    output stallF,
+    output stallD,
+    output flushD,
+    output flushE
+);
+    
+endmodule
