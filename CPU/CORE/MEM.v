721f9ef95cd3d379098c23e442ee0a8bae102bce
YatagacLapotk <yatagac.ktopal@gmail.com>
2026-04-05 12:10:42 +0300

    MEM bitirildi. Test edilecek.

--- CPU/CORE/MEM.v
+++ CPU/CORE/MEM.v
@@ -5,11 +5,53 @@ module MEM (
     input reset,
     input [`DATA_WIDTH-1:0] execute_result_in,
     input [`DATA_WIDTH-1:0] mem_write_data,
+    input [`WB_CNTRL-1:0] wb_controlM,
     input reg_write,
     input mem_write,
     input [`ADDRESS_WIDTH-1:0] rdM,
-    output [`ADDRESS_WIDTH-1:0] rdW,
-    output [`DATA_WIDTH-1:0] execute_result_out
+    output [`ADDRESS_WIDTH-1:0] rdM_hazard_out,
+    output reg_write_hazard,
+    output reg reg_write_out,
+    output reg [`WB_CNTRL-1:0] wb_control_out,
+    output reg [`ADDRESS_WIDTH-1:0] rdW,
+    output [`DATA_WIDTH-1:0] execute_result_out,
+    output reg [`DATA_WIDTH-1:0] mem_result_out,
+    output reg [`DATA_WIDTH-1:0] wb_result_out
+
 );
-    
+//Buraya D_cache ekledim ancak tam olarak bizim tasarıma uymuyor sanırım 
+// bunun için ne yapmamız gerekir onu bilmiyorum. 
+// Mesela eğer bir veriyolu içerisine koyarsak sistem yavaşlar mı?
+wire [`DATA_WIDTH-1:0] mem_result_out_temp;
+
+D_CACHE D_CACHE (
+    .clk(clk),
+    .reset(reset),
+    .we(mem_write),
+    .data_in(mem_write_data),
+    .w_addr(execute_result_in),
+    .r_addr(execute_result_in),
+    .data_out(mem_result_out_temp)
+);
+
+always @(posedge clk) begin
+    if(reset) begin
+        reg_write_out <= 0;
+        wb_control_out <= 0;
+        rdW <= 0;
+        mem_result_out <= 0;
+        wb_result_out <= 0;
+    end else begin
+        reg_write_out <= reg_write;
+        wb_control_out <= wb_controlM;
+        rdW <= rdM;
+        mem_result_out <= mem_result_out_temp;
+        wb_result_out <= exe_result_in;
+    end
+end
+
+assign execute_result_out = execute_result_in;
+assign rdM_hazard_out = rdM;
+assign reg_write_hazard = reg_write;
+
 endmodule
