14857ba176d9dfb7e6993a6d9a1406fa3f6e7a9b
YatagacLapotk <you@example.com>
2026-04-23 18:12:46 +0300

    Genel düzeltmeler ve yenilemeler yapıldı.

---
+++ CPU/CORE/CORE.v
@@ -0,0 +1,9 @@
+`include "/Users/yatagaclapotk/Desktop/Genel_Calismalar/Mezuniyet/Mezuniyet_Projesi/CPU/SABIT_VERILER/sabit_veriler.vh"
+
+module CORE (
+    input clk,
+    input reset,
+    
+);
+    
+endmodule
